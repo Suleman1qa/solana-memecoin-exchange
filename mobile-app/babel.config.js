bbae0c11762a725bfb5efb5670f3fde6e20a89c8
dodelulu <suleman.shujah@gmail.com>
2025-06-23 21:10:28 +0500

    Update babel.config.js: Babel plugin configuration adjustments

--- mobile-app/babel.config.js
+++ mobile-app/babel.config.js
@@ -1,20 +1,25 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-        blocklist: null,
-        allowlist: null,
-        safe: false,
-        allowUndefined: true,
-        verbose: false,
-      },
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
+      ['@babel/plugin-transform-class-properties', { loose: false }],
+      ['@babel/plugin-transform-private-methods', { loose: false }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: false }],
+        'react-native-reanimated/plugin',
+
     ],
-    ['@babel/plugin-transform-class-properties', { loose: false }],
-    ['@babel/plugin-transform-private-methods', { loose: false }],
-    ['@babel/plugin-transform-private-property-in-object', { loose: false }],
-  ],
+  };
 };
