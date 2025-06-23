461680531716a7c35368596d2e0be5862afbba1e
dodelulu <suleman.shujah@gmail.com>
2025-06-23 21:10:28 +0500

    Update babel.config.js: Babel plugin configuration adjustments

--- mobile-app/index.js
+++ mobile-app/index.js
@@ -1,12 +1,18 @@
+import 'react-native-gesture-handler';
+import React from 'react';
 import { registerRootComponent } from 'expo';
 import App from './src/App.js';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider as PaperProvider } from 'react-native-paper';
 
-registerRootComponent(
-  <SafeAreaProvider>
-    <PaperProvider>
-      <App />
-    </PaperProvider>
-  </SafeAreaProvider>
-);
+function Root() {
+  return (
+    <SafeAreaProvider>
+      <PaperProvider>
+        <App />
+      </PaperProvider>
+    </SafeAreaProvider>
+  );
+}
+
+registerRootComponent(Root);
